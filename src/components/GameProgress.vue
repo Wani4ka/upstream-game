1e6547d23b82aceee46c2ebabf2120412d7f08cf
Ivan Lutsenka <wani4ka@duck.com>
2023-03-10 13:13:10 +0300

    Add progress bar

---
+++ src/components/GameProgress.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="flex flex-row flex-wrap max-w-full gap-y-1 justify-center mt-5">
+		<span
+			v-for="index in (to-from+1)"
+			:key="index"
+			class="text-center w-[30px]"
+			:class="{
+				'bg-gradient-to-b': true,
+				'via-blue-800 from-blue-700 to-blue-700':
+					(!shouldApplyCustomState(index) && index > max) || (shouldApplyCustomState(index) && customState === 'blue'),
+				'via-yellow-800 from-yellow-700 to-yellow-700':
+					(!shouldApplyCustomState(index) && index >= current && index <= max) || (shouldApplyCustomState(index) && customState === 'yellow'),
+				'via-lime-800 from-lime-700 to-lime-700': (!shouldApplyCustomState(index) && index < current) || (shouldApplyCustomState(index) && customState === 'lime'),
+				'via-red-800 from-red-700 to-red-700': shouldApplyCustomState(index) && customState === 'red',
+			}"
+		><div v-if="index === current" class="z-10 absolute w-[4px] bg-blue-100 h-full"/>{{ from + index - 1 }}</span>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	from: {
+		type: Number,
+		required: true
+	},
+	to: {
+		type: Number,
+		required: true
+	},
+	current: {
+		type: Number,
+		required: true
+	},
+	max: {
+		type: Number,
+		required: true
+	},
+	customState: String
+})
+
+let shouldApplyCustomState = (index:number): boolean => !!props.customState && index === props.current
+</script>
--- src/components/TheGame.vue
+++ src/components/TheGame.vue
@@ -10,15 +10,23 @@
 			:left="questions[currentQuestion].left"
 			:right="questions[currentQuestion].right"
 			:incorrect="questions[currentQuestion].incorrect"
-			@proceed="openQuestion(currentQuestion + 1)"
-			@fail="openQuestion(0)"
+			@proceed="openQuestion(currentQuestion + 1, 'lime')"
+			@fail="openQuestion(0, 'red')"
 		></GameOptions>
+		<GameProgress
+			:custom-state="questionProgressState"
+			:max="maxAnswers"
+			:current="currentQuestion + 1"
+			:from="1"
+			:to="questions.length"
+			v-if="showQuestionNumbers" />
 	</div>
 </template>
 
 <script setup lang="ts">
 import GameQuestion from '@/components/GameQuestion.vue'
 import GameOptions from '@/components/GameOptions.vue'
+import GameProgress from './GameProgress.vue'
 import type { ParsedGameQuestion } from '@/declarations/question'
 import { ref } from 'vue'
 
@@ -34,6 +42,8 @@ const props = defineProps({
 })
 const emit = defineEmits(['win', 'fail'])
 let currentQuestion = ref(0)
+let maxAnswers = ref(0)
+let questionProgressState = ref('')
 let options = ref()
 
 let checkQuestionNum = () => {
@@ -44,9 +54,12 @@ let checkQuestionNum = () => {
 	}
 }
 
-function openQuestion(val: number) {
+function openQuestion(val: number, tempState: string = '') {
+	questionProgressState.value = tempState
 	setTimeout(() => {
+		maxAnswers.value = Math.max(maxAnswers.value, currentQuestion.value)
 		currentQuestion.value = val
+		questionProgressState.value = ''
 		options.value.reset()
 		checkQuestionNum()
 	}, 500)
