6e6457edb73ac687209958c9cde6be1061dd3e43
Ivan Lutsenka <wani4ka@duck.com>
2023-03-13 04:08:43 +0300

    Fix hover gradient

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -7,17 +7,17 @@
 				'bg-gradient-to-b': true,
 				'via-blue-900 from-blue-700 to-blue-700 border-blue-600':
 					state === 'default' || !state,
-				'hover:from-blue-600 hover:to-blue-600':
+				'hover:from-blue-600 hover:via-blue-800 hover:to-blue-600':
 					hoverable && (state === 'default' || !state),
 				'via-yellow-900 from-yellow-700 to-yellow-700 border-yellow-600':
 					state === 'selected',
-				'hover:from-yellow-600 hover:to-yellow-600':
+				'hover:from-yellow-600 hover:via-yellow-800 hover:to-yellow-600':
 					hoverable && state === 'selected',
 				'via-lime-900 from-lime-700 to-lime-700 border-lime-600': state === 'incorrect',
-				'hover:from-lime-600 hover:to-lime-600':
+				'hover:from-lime-600 hover:via-lime-800 hover:to-lime-600':
 					hoverable && state === 'incorrect',
 				'via-red-900 from-red-700 to-red-700 border-red-600': state === 'correct',
-				'hover:from-red-600 hover:to-red-600':
+				'hover:from-red-600 hover:via-red-800 hover:to-red-600':
 					hoverable && state === 'correct',
 			}"
 		></span>
